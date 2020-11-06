eb5e740fe5a7a1a8d8d34474e418ee63a3cfb60d
Mohammed Khellafi <61737298+mohakhlf@users.noreply.github.com>
2020-11-06 22:39:49 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Contact from "./Contact";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Contact />
+    </div>
+  );
+}
