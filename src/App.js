d34bf8ecdac41bc503dcb3649912e9776d374086
Mohammed <Mohammed.K@itdm-gitlab.ssii.be>
2020-11-08 16:36:35 +0100

    fake data in data.json + map data with component Contact

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
+import data from './data.json'
 import Contact from "./Contact/Contact";
 import "./styles.css";
 
-const BASE_URL = "https://dummyapi.io/data/api";
-const APP_ID = "5fa6c9b8d1eb0a385ca97203";
-
-let con = new Array();
+const contact = data.map( (item) =>
+    <Contact
+        key={item.id}
+        first={item.firstname}
+        last={item.lastname}
+        image={item.image}
+        online={Math.random() >= 0.5}
+    />
+)
 
 export default function App() {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState(null);
 
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get(`${BASE_URL}/user`, { headers: { "app-id": APP_ID } })
-      .then(({ data }) => setData(data))
-      .catch(console.error)
-      .finally(() => setLoading(false));
-  }, []);
-
-  con = data.data.map((obj) => (
-    <Contact
-      first={obj.firstName}
-      last={obj.lastName}
-      image={obj.picture}
-      online={Math.random() >= 0.5}
-    />
-  ));
 
   return (
     <div className="App">
-      {loading && "Loading..."}
-      {con}
+        {contact}
     </div>
   );
 }
